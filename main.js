5ac76872bfe2897ab456f161dd61346c4725b29e
neketa <nekit53@gmail.com>
2017-04-25 19:09:57 +0300

    add a coments

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
+
+//Declare two variables and show them
 var a = 1;
 var b = 'string';
 
 console.log('a = ' + a);
 console.log('b = ' + b);
+
+//Write a function that reverse the given array using three kind of loops
 
 var myArray = [1, 2, 3, 4];
 
@@ -38,6 +42,18 @@ function reverseArrayViaDoWhile(arr) {
     return result;
 };
 
+//Explain the difference between ++i and i++ with example
+var i = 3;
+a = i++ * 2;
+
+var i = 3;
+b = ++i * 2;
+console.log("i = 3");
+console.log("a = i++ * 2 = " + a);
+console.log("b = ++i * 2 = " + b);
+
+
+//Write a function that checks if the given arguments is positive number or negative number or is 0
 function numberCheck(num) {
     if (num > 0) {
         console.log(num + ' > 0');
@@ -47,6 +63,8 @@ function numberCheck(num) {
         console.log(num + ' = 0');
     }
 };
+
+//Write a function that calculates factorial
 
 function factorial(num) {
     var result = 1;
@@ -59,6 +77,12 @@ function factorial(num) {
 };
 
 console.log(factorial(4));
+
+//Create a page that asks the user his name and alert it back
+var name = prompt("Your name?");
+alert("Your name is " + name);
+
+//Write a function that can find triangle's area
 
 function triangleAria(a, b, c) {
     var perimeter = a + b + c;
